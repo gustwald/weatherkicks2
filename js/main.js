c40fb716367f7e6f622bd59d344a5b8d2e90ae9a
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-03-14 13:29:47 +0100

    outputsbasedonweather

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 
             var date = base.formatDate(weather.currently.time);
             var temperature = Math.round(weather.currently.temperature);
+            var weatherIcons = weather.currently.icon;
             var summary = weather.currently.summary;
             var icons = [
                 "clear-day",
@@ -17,10 +18,40 @@ $( document ).ready(function() {
                 "wind",
                 "fog"
             ]
-    
-            if(icons.indexOf(weather.currently.icon) > -1);{    
-                $(".output").addClass(weather.currently.icon);
+            var badWeather = [
+                "Don't flex your AF1's today.",
+                "I wouldn't go out with your OG's today.",
+                "Get your beaters"
+            ]
+            var goodWeather = [
+                "Your shoes are fine today.",
+                "Flex your OG's today.",
+                "Suns out, grails out",
+            ]
+            var nightTime = [
+                "Night, but kicks should be fine though.",
+                "Bedtime player",
+                "Hope you got your kicks tucked in",
+                "Sneakers has to rest too"
+            ]
+            var randomGoodTip = goodWeather[Math.floor(Math.random()*goodWeather.length)];
+            var randomBadTip = badWeather[Math.floor(Math.random()*badWeather.length)];
+            var randomNightTip = nightTime[Math.floor(Math.random()*nightTime.length)];
+
+            if(icons.indexOf(weatherIcons) > -1){    
+                $(".output").addClass(weatherIcons);
+            }else{
+                $(".output").text("Sorry, we could not find any weather :()");
             }
+
+            if(weatherIcons.includes("night")){
+                $(".tips").text(randomNightTip);
+            }else if(weatherIcons.includes("rain sleet snow")){
+                $(".tips").text(randomBadTip);
+            }else{
+                $(".tips").text(randomGoodTip);
+            }
+            
             
             $(".container").fadeIn("slow");
             $(".loader").fadeOut("fast");
