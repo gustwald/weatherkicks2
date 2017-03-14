415adb75465b2cd3fa752d6eef5690acead5eef0
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-03-14 16:24:59 +0100

    added timeline 6forthcoming hours

--- js/main.js
+++ js/main.js
@@ -21,18 +21,22 @@ $( document ).ready(function() {
             var badWeather = [
                 "Don't flex your AF1's today.",
                 "I wouldn't go out with your OG's today.",
-                "Get your beaters"
+                "Get your beaters.",
+                "Your kick will get dirty.",
+                "Stay inside if you wanna flex.",
+                "Stay inside."
             ]
             var goodWeather = [
                 "Your shoes are fine today.",
                 "Flex your OG's today.",
-                "Suns out, grails out",
+                "Suns out, grails out.",
+                "Good condition for flexing."
             ]
             var nightTime = [
                 "Night, but kicks should be fine though.",
-                "Bedtime player",
-                "Hope you got your kicks tucked in",
-                "Sneakers has to rest too"
+                "Bedtime player.",
+                "Hope you got your kicks tucked in.",
+                "Sneakers has to rest too."
             ]
             var randomGoodTip = goodWeather[Math.floor(Math.random()*goodWeather.length)];
             var randomBadTip = badWeather[Math.floor(Math.random()*badWeather.length)];
@@ -55,8 +59,13 @@ $( document ).ready(function() {
             
             $(".container").fadeIn("slow");
             $(".loader").fadeOut("fast");
-            $('.temperature').html(temperature + '&#8451;, ' + summary);
+            $('.temperature').html(temperature + '&deg;C, ' + summary);
 
+            $.each(weather.hourly.data, function( key, value ) {
+               if(key < 7){
+                 $('.timeline').append('<li class="li hour"><div class="timestamp"><span class="time">'+base.formatDate(value.time) + '</span></div><div class="status"><span class="degree">'+ Math.round(value.temperature) +'&deg;C</span></div>');
+               }
+        });
 
             console.log(weather);
             console.log(date);
