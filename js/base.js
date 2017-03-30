1f42590abf15d366fa561a83a22ff019988277d9
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-03-30 15:03:43 +0200

    cleanup

--- js/base.js
+++ js/base.js
@@ -63,6 +63,96 @@ var base = {};
             var formattedTime = hours + ':' + minutes.substr(-2);
 
             return formattedTime;
+        },
+        addWeatherClass: function(weatherIcon){
+             var icons = [
+                "clear-day",
+                "clear-night",
+                "partly-cloudy-day",
+                "partly-cloudy-night",
+                "cloudy",
+                "rain",
+                "sleet",
+                "snow",
+                "wind",
+                "fog"
+            ]
+              //checks which icon the weatherdata currently has and adds that to class
+            if(icons.indexOf(weatherIcon) > -1){    
+                $(".output").addClass(weatherIcon);
+            }else{
+                $(".output").text("Sorry, we could not find any weather :()");
+            }
+        },
+        decideOutput: function(weatherIcon){
+             var badWeather = [
+                "Don't flex your AF1's today.",
+                "I wouldn't go out with your OG's.",
+                "Get your beaters.",
+                "Your kicks will get dirty.",
+                "Stay inside if you wanna flex.",
+                "Stay inside."
+            ]
+            var goodWeather = [
+                "Your shoes are fine.",
+                "Flex your OG's.",
+                "Suns out, grails out.",
+                "Good condition for flexing."
+            ]
+            var nightTime = [
+                "Night, but kicks should be fine though.",
+                "Bedtime player.",
+                "Hope you got your kicks tucked in.",
+                "Sneakers has to rest too."
+            ]
+            var randomGoodTip = goodWeather[Math.floor(Math.random()*goodWeather.length)];
+            var randomBadTip = badWeather[Math.floor(Math.random()*badWeather.length)];
+            var randomNightTip = nightTime[Math.floor(Math.random()*nightTime.length)];
+
+              //decides which output the app should have
+            if(weatherIcon.includes("night")){
+                $(".tips").text(randomNightTip);
+            }else if(weatherIcon.includes("rain") || weatherIcon.includes("snow") || weatherIcon.includes("sleet")){
+                $(".tips").text(randomBadTip);
+            }else{
+                $(".tips").text(randomGoodTip);
+            }
+        },
+        appendTimeline: function(weather){
+             //Creates the timeline for weather coming 7 hours.
+            $.each(weather.hourly.data, function( key, value ) {
+               if(key < 7){
+                 $('.timeline').append('<li class="li hour"><div class="timestamp"><span class="time">'+base.formatDate(value.time) + '</span></div><div class="status"><span class="degree">'+ Math.round(value.temperature) +'&deg;C</span></div></li>');
+               }
+            });
+        },
+        loaderAndShowData: function(summary, temperature){
+            $(".container").fadeIn("slow");
+            $(".loader").fadeOut("fast");
+            $('.temperature').html('<strong>' + temperature + '&deg;C</strong>, ');
+            $('.summary').html(summary);
+        },
+        appendWeatherFarenheit: function(){
+             $('.change').fadeOut();
+             $('.sk-circle').fadeIn();
+        
+             base.getCoordinates().then(function(coordinates){
+                base.getWeatherFarenheit(coordinates.coords).then(function(weatherFarenheit){
+                $('.sk-circle').fadeOut();
+
+                $('.temperature').fadeOut(1000, function(){
+                $('.temperature').empty().append('<strong>' + Math.round(weatherFarenheit.currently.temperature) + '&deg;F, </strong>').fadeIn();
+                })
+
+                $.each(weatherFarenheit.hourly.data, function( key, value ) {
+                            if(key < 7){
+                            $('.degree').fadeOut(1000, function(){
+                            $('.degree').eq(key).empty().append(Math.round(value.temperature) + '&deg;F').fadeIn();
+                         });
+                        }
+                    });
+                })
+            })
         }
    }
 })();
